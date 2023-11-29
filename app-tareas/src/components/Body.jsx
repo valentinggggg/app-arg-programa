b5ddeebbf59562ea20c87fa5035da2c30e86abd8
valentinggggg <garellovalentin@gmail.com>
2023-11-29 19:41:17 -0300

    avance
    dsada

---
+++ app-tareas/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import './Body.css'
+
+export default function Body() {
+
+    return(
+        <div className="Body-container">
+            <h1 className="text-5xl font-bold text-center">Lista de Tareas Online</h1>
+            <div className="ListaTareas">
+            
+
+            </div>
+        </div>
+    )
+}

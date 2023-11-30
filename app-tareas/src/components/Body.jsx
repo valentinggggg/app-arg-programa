402a25ce0ebfd21696e59ce9fc22d6b021b99a21
valentinggggg <garellovalentin@gmail.com>
2023-11-30 00:39:57 -0300

    avances
    avances

--- app-tareas/src/components/Body.jsx
+++ app-tareas/src/components/Body.jsx
@@ -1,14 +1,35 @@
 import './Body.css'
+import { useState } from 'react'
 
 export default function Body() {
 
-    return(
-        <div className="Body-container">
-            <h1 className="text-5xl font-bold text-center">Lista de Tareas Online</h1>
-            <div className="ListaTareas">
-            
+    const [nuevaTarea, setNuevaTarea] = useState ()
 
-            </div>
+   const SubmitTask = (e) => {
+        e.preventDefault();
+        localStorage.setItem("Tarea", nuevaTarea)
+        setNuevaTarea('')
+    }
+ 
+
+
+    return(
+    <div className="Body-container">
+        <h1 className="text-5xl font-bold text-center">Lista de Tareas Online</h1>
+        <div className="ListaTareas">
+            <form onSubmit={SubmitTask}>
+                <input
+                className='inputTask'
+                type='text'
+                placeholder='¿Que queres hacer?'
+                value={nuevaTarea}
+                onChange={(event) => setNuevaTarea(event.target.value)}
+                />
+                <button className='buttonTask'>
+                Agregar Tarea
+                </button>
+            </form>
         </div>
+    </div>
     )
 }
